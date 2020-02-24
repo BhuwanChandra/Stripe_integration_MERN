f0b939d27f212555f46e495de07186e84cd4c809
BhuwanChandra <bhuwan7779@gmail.com>
2020-02-24 22:32:39 +0530

    added backend code

--- stripebackend/index.js
+++ stripebackend/index.js
@@ -1,7 +1,8 @@
 const cors = require('cors');
 const express = require('express');
-// TODO: add a stripe key
-const stripe = require('stripe');
+
+require('dotenv').config();
+const stripe = require("stripe")(process.env.SECRET_KEY);
 const uuid = require('uuid/v4');
 
 const app = express();
@@ -16,6 +17,37 @@ app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("THIS IS THE HOME PAGE !!! ");
+})
+
+app.post('/payment', (req, res) => {
+
+    const {product, token} = req.body;
+    console.log("PRODUCT",product);
+    console.log("PRICE",product.price);
+    const idempotencyKey = uuid();
+
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: token.email,
+            description: product.name,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    country: token.card.address_country
+                }
+            }
+        }, {idempotencyKey})
+    })
+    .then(result => res.status(200).json(result))
+    .catch(err => console.log(err));
+    
+ 
 })
 
 
